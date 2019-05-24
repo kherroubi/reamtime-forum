dcab0075315ed7855fe2540a6b98dd0915a7f9c2
Chafik <kherroubi.chafik@gmail.com>
2019-05-25 01:23:26 +0200

    CRUD Question using Question Resource

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,82 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+    public function index(){
+        return QuestionResource::collection(Question::latest()->get());    
+    }
+
+    public function store(Request $request){
+        $question = Question::create($request->all());
+        return $question;
+    }
+
+    public function show(Question $question){
+        return new QuestionResource($question);
+    }
+    public function edit(Question $question){
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+    public function update(Request $request, Question $question){
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Question $question)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Question $question)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Question $question)
-    {
-        //
+    public function destroy(Question $question){
+        $question->delete();
+        return response('Question deleted', 202);
     }
 }
---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    public function toArray($request){
+        return [
+            'url'       =>  $this->url,
+            'title'     =>  $this->title,
+            'content'   =>  $this->content,
+            'user'      =>  $this->user->name,
+            'created_at'=>  $this->created_at->diffForHumans()
+        ];
+    }
+}
